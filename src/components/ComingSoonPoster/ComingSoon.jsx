1c67032b7ed125e16da6e562d1c1d48170e8c678
Sharath Chandra Chinnarigari <csharathchandra3@gmail.com>
2024-03-20 00:55:57 +0530

    CS added

--- src/components/ComingSoonPoster/ComingSoon.jsx
+++ src/components/ComingSoonPoster/ComingSoon.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import Poster from "./comingsoon.png";
+import Poster from "./CS.jpg";
 
 const ComingSoon = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div
+      className="flex justify-center items-center"
+      style={{ width: "100vw", height: "100vh", background: "#131424" }}
+    >
       <img src={Poster} alt="Coming Soon" className="max-w-full max-h-full" />
     </div>
   );
