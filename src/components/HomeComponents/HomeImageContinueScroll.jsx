57672a36448365c1d692a96ed4615d1d15433fed
Sharath Chandra Chinnarigari <csharathchandra3@gmail.com>
2024-04-08 12:47:17 +0530

    home Image

--- src/components/HomeComponents/HomeImageContinueScroll.jsx
+++ src/components/HomeComponents/HomeImageContinueScroll.jsx
@@ -1,34 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
-import image from '../../static/images/home_carousel_image.png';
+import image from "../../static/images/Companies_Banner.png";
 
 const HomeImageContinueScroll = () => {
-    // make a useEffect for window resize and change the speed of the marquee
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            if (window.innerWidth < 768) {
-                setIsMobile(true);
-            } else {
-                setIsMobile(false);
-            }
-        });
-    }, []);
+  // make a useEffect for window resize and change the speed of the marquee
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    });
+  }, []);
 
-
-    return (
-        <Marquee
-            speed={isMobile ? 7 : 20}
-            gradient={isMobile ? false : true}
-            pauseOnHover={true}
-
-        >
-            <img src={image} className='h-[300px] md:h-[350px] object-cover mx-[28px]' alt="home_carousel_image" />
-            <img src={image} className='h-[300px] md:h-[350px] object-cover mx-[28px]' alt="home_carousel_image" />
-            <img src={image} className='h-[300px] md:h-[350px] object-cover mx-[28px]' alt="home_carousel_image" />
-
-        </Marquee>
-    );
+  return (
+    <Marquee
+      speed={isMobile ? 22 : 32}
+      gradient={isMobile ? false : true}
+      pauseOnHover={true}
+    >
+      <img
+        src={image}
+        className="h-[100px] md:h-[150px] object-cover mx-[28px]"
+        alt="home_carousel_image"
+      />
+      <img
+        src={image}
+        className="h-[100px] md:h-[150px] object-cover mx-[28px]"
+        alt="home_carousel_image"
+      />
+      <img
+        src={image}
+        className="h-[100px] md:h-[150px] object-cover mx-[28px]"
+        alt="home_carousel_image"
+      />
+    </Marquee>
+  );
 };
 
 export default HomeImageContinueScroll;
